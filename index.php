883fcdce3ebf292964269c2353ac06cde33a7937
ShyroVO <bosquet.laurie@outlook.com>
2021-01-19 11:39:22 +0100

    1.0

--- index.php
+++ index.php
@@ -2,21 +2,24 @@
 
 // Premiere ligne
 
-{
+function maFonction (){
     echo "Hello World!";
 }
 
 //Deuxieme ligne
 
-
+maFonction();
 
 //Troisieme ligne
 
 function maFonctionParam($fname, $lname) {
-    echo ;
+    echo $fname;
+    return $lname;
 }
 
+maFonctionParam('truc', 'trucDeux');
 
+?>
 
 
 
